cac628ec25e702136b059f770c094ab2e4caa6ba
Kyle <kyle@kyleperry.dev>
2024-07-16 21:17:03 -0600

    chore: update nav

--- src/components/headerNav.tsx
+++ src/components/headerNav.tsx
@@ -1,13 +1,64 @@
+"use client";
 import Link from "next/link";
 import { FC } from "react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const HeaderNav: FC = () => {
+  const path = usePathname();
+  console.log(path);
+
+  enum PATHS {
+    HOME = "/",
+    ABOUT = "/about",
+    PROJECTS = "/projects",
+    RESUME = "/resume",
+  }
+
+  const isPathActive = (pathName: string) => pathName === path;
+
   return (
     <div className="flex gap-8 p-4 bg-sky-800 text-white">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/resume">Resume</Link>
+      <button
+        className={clsx(
+          "flex justify-items-center rounded px-4 cursor-pointer",
+          {
+            "bg-sky-500": isPathActive(PATHS.HOME),
+          }
+        )}
+      >
+        <Link href={PATHS.HOME}>Home</Link>
+      </button>
+      <button
+        className={clsx(
+          "flex justify-items-center rounded px-4 cursor-pointer",
+          {
+            "bg-sky-500": isPathActive(PATHS.ABOUT),
+          }
+        )}
+      >
+        <Link href={PATHS.ABOUT}>About</Link>
+      </button>
+      <button
+        className={clsx(
+          "flex justify-items-center rounded px-4 cursor-pointer",
+          {
+            "bg-sky-500": isPathActive(PATHS.PROJECTS),
+          }
+        )}
+      >
+        <Link href={PATHS.PROJECTS}>Projects</Link>
+      </button>
+      <button
+        className={clsx(
+          "flex justify-items-center rounded px-4 cursor-pointer",
+          {
+            "bg-sky-500": isPathActive(PATHS.RESUME),
+          }
+        )}
+      >
+        <Link href={PATHS.RESUME}>Resume</Link>
+      </button>
     </div>
   );
 };
